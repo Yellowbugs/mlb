0b59e924abc5ce2a6383e76447f2ccdce367bc8b
Yellowbugs <49653665+Yellowbugs@users.noreply.github.com>
2023-06-05 17:10:51 -0400

    1.2

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import statsapi
 import json
 import requests
+import datetime
+from datetime import timedelta, date
+import re
 from oddsapi import OddsApiClient
 teamIds = {
     'Los Angeles Angels': 108,
     'Arizona Diamondbacks':109,
     'Baltimore Orioles': 110,
-    'Boston Red Sox	': 111,
+    'Boston Red Sox': 111,
     'Chicago Cubs': 112,
     'Cincinnati Reds': 113,
     'Cleveland Guardians': 114,
@@ -35,43 +38,73 @@ teamIds = {
     'Milwaukee Brewers': 158,
 
 }
+projections = {}
 
 client = OddsApiClient(api_key='b664d59f43cfee882082f8fb86a1c478')
-odds_response = requests.get('https://api.the-odds-api.com/v3/odds', params={
-    'api_key': 'b664d59f43cfee882082f8fb86a1c478',
-    'sport': 'baseball_mlb',
-    'region': 'us', # uk | us | eu | au
-    'oddsFormat': 'american',
-    "mkt": "totals",
-    "bookmakers": [
-        {
-        "key": "draftkings",
-        "title": "DraftKings",    
-  
-    }]
-})
-odds_json = json.loads(odds_response.text)
-print(odds_json['data'])
-print("odds:")
-for x in range(len(odds_json['data'])):
-    print(odds_json['data'][x]['teams'])
-    print(odds_json['data'][x]['sites'][0]['odds']['totals']['points'][0])
+
+def simulateday(date,today,datex, startdate):
+    for i in today:
+        gameCount = 0
+        totalRuns = 0
+        teamId = teamIds[str(i['home_name'])]
+        games = statsapi.schedule(start_date=startdate,end_date= datex,team=teamId)
+        for x in games:
+            if x['status'] == 'Final':
+                gameCount = gameCount + 1
+                #print(x['summary'])
+                #print(x['home_score'] + x['away_score'])
+                totalRuns = totalRuns + x['home_score'] + x['away_score']
+        prediction = totalRuns/gameCount
+        projections.update({i['home_name']: prediction})
+
+    for i in today:
+        gameCount = 0
+        totalRuns = 0
+        teamId = teamIds[str(i['away_name'])]
+        games = statsapi.schedule(start_date=startdate,end_date= datex,team=teamId)
+        for x in games:
+            if x['status'] == 'Final':
+                gameCount = gameCount + 1
+                #print(x['summary'])
+                #print(x['home_score'] + x['away_score'])
+                totalRuns = totalRuns + x['home_score'] + x['away_score']
+        prediction = totalRuns/gameCount
+        projections.update({i['away_name']: prediction})
+    odds_response = requests.get('https://api.the-odds-api.com/v3/odds', params={
+        'api_key': 'b664d59f43cfee882082f8fb86a1c478',
+        'sport': 'baseball_mlb',
+        'region': 'us', # uk | us | eu | au
+        'oddsFormat': 'american',
+        "mkt": "totals",
+        "bookmakers": [
+            {
+            "key": "draftkings",
+            "title": "DraftKings",    
+    
+        }]
+    })
+    print('date: ', datex)
+    odds_json = json.loads(odds_response.text)
+    for x in range(len(odds_json['data'])):
+        dt = datetime.datetime.fromtimestamp(odds_json['data'][x]['commence_time'])
+        iso_format = dt.isoformat()
+        if iso_format[:10] == date:
+            print("game: " , odds_json['data'][x]['teams'])
+            odds = odds_json['data'][x]['sites'][0]['odds']['totals']['points'][0]
+            print("odds: " , odds)
+            projectedTotal = (projections[str(odds_json['data'][x]['teams'][0])]+projections[str(odds_json['data'][x]['teams'][1])])/2
+            print("projected total: " , projectedTotal)
+            if projectedTotal <= odds:
+                play = "under"
+            else:
+                play = "over"
+            print("play: ", play)
+            
 
 
-print("projections:")
-g = statsapi.schedule(date='06/06/2023')
-
-for i in g:
-    gameCount = 0
-    totalRuns = 0
-    print(i['home_name'])
-    teamId = teamIds[str(i['home_name'])]
-    #print(teamId)
-    games = statsapi.schedule(start_date='06/01/2023',end_date='06/05/2023',team=teamId)
-    for x in games:
-        if x['status'] == 'Final':
-            gameCount = gameCount + 1
-            #print(x['summary'])
-            #print(x['home_score'] + x['away_score'])
-            totalRuns = totalRuns + x['home_score'] + x['away_score']
-    print(totalRuns/gameCount)
+today = date.today()    
+datex = re.sub(r'(\d{4})-(\d{1,2})-(\d{1,2})', '\\2/\\3/\\1', str(today))
+n_days_ago = re.sub(r'(\d{4})-(\d{1,2})-(\d{1,2})', '\\2/\\3/\\1', str(today - timedelta(days=10)))
+#print(today)
+#print(datex, n_days_ago)
+simulateday(str(today),statsapi.schedule(date=str(today)), datex, n_days_ago)
